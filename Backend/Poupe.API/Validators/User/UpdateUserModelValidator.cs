b37993bd8d0933e57f4224328cb7d47b0b89f61b
Lucas <soares0lucas8@gmail.com>
2025-12-14 18:19:53 -0300

    feat: validação com FluentValidation

---
+++ Backend/Poupe.API/Validators/User/UpdateUserModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Poupe.API.Models.User;
+
+namespace Poupe.API.Validators.User;
+
+public class UpdateUserModelValidator : AbstractValidator<UpdateUserModel>
+{
+    public UpdateUserModelValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Age)
+            .NotNull()
+            .GreaterThanOrEqualTo(0);
+    }
+}
