53e1ea3b105e0668f775fad9b50b52fb82d0349b
Lucas <soares0lucas8@gmail.com>
2025-12-14 18:22:02 -0300

    feat: entidade de pessoas

---
+++ Backend/Poupe.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Poupe.Domain.Entities;
+
+[Table("tb_user", Schema = ("PoupeDB"))]
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public Guid? Id { get; set; }
+
+    [Column("identifier")]
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = null!;
+
+    [Column("age")]
+    [Required]
+    public int Age { get; set; }
+}
