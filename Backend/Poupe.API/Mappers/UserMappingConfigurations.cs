fa35e1ab0571703656e7e29b767dac3fc0e27da4
Lucas <soares0lucas8@gmail.com>
2025-12-14 18:18:51 -0300

    feat: mapeamento com Mapster

---
+++ Backend/Poupe.API/Mappers/UserMappingConfigurations.cs
@@ -0,0 +1,38 @@
+﻿using Mapster;
+using Poupe.API.Models.User;
+using Poupe.Domain.DTOs.User;
+using Poupe.Domain.Entities;
+
+namespace Poupe.API.Mappers;
+
+public static class UserMappingConfigurations
+{
+    public static void RegisterUserMaps(this IServiceCollection services)
+    {
+        TypeAdapterConfig<CreateUserModel, UserCreateDTO>
+            .NewConfig()
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Age, src => src.Age);
+
+        TypeAdapterConfig<UserCreateDTO, User>
+            .NewConfig()
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Age, src => src.Age);
+
+        TypeAdapterConfig<User, UserResponseDTO>
+            .NewConfig()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Age, src => src.Age);
+
+        TypeAdapterConfig<UpdateUserModel, UserUpdateDTO>
+            .NewConfig()
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Age, src => src.Age);
+
+        TypeAdapterConfig<UserUpdateDTO, User>
+            .NewConfig()
+            .Map(dest => dest.Name, src => src.Name)
+            .Map(dest => dest.Age, src => src.Age);
+    }
+}
