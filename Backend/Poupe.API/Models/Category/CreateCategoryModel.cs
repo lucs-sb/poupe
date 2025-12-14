51d9855072f387ec3f2da5e48ead912341b666a4
Lucas <soares0lucas8@gmail.com>
2025-12-14 20:14:58 -0300

    feat: models e dtos de categoria

---
+++ Backend/Poupe.API/Models/Category/CreateCategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using Poupe.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace Poupe.API.Models.Category;
+
+public class CreateCategoryModel
+{
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("purpose")]
+    public CategoryType? Purpose { get; set; }
+}
---
+++ Backend/Poupe.Domain/DTOs/Category/CategoryResponseDTO.cs
@@ -0,0 +1,7 @@
+﻿using Poupe.Domain.Enums;
+
+namespace Poupe.Domain.DTOs.Category;
+
+public record CategoryResponseDTO (string Id, string Description, CategoryType Purpose)
+{
+}
