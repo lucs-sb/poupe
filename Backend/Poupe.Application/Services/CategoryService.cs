2f2b9f24c6f060a7cc6428afc143fdc59985c6b2
Lucas <soares0lucas8@gmail.com>
2025-12-14 20:16:32 -0300

    feat: camada de serviço de categoria

---
+++ Backend/Poupe.Application/Services/CategoryService.cs
@@ -0,0 +1,96 @@
+﻿using Mapster;
+using Poupe.Application.Resources;
+using Poupe.Domain.DTOs.Category;
+using Poupe.Domain.Entities;
+using Poupe.Domain.Interfaces;
+using Poupe.Domain.Interfaces.Repositories.Base;
+
+namespace Poupe.Application.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<CategoryResponseDTO> CreateAsync(CategoryCreateDTO categoryCreateDTO)
+    {
+        await _unitOfWork.BeginTransactionAsync();
+
+        try
+        {
+            Category category = categoryCreateDTO.Adapt<Category>();
+
+            await _unitOfWork.Repository<Category>().AddAsync(category);
+
+            await _unitOfWork.CommitAsync();
+
+            return category.Adapt<CategoryResponseDTO>();
+        }
+        catch
+        {
+            await _unitOfWork.RollbackAsync();
+
+            throw;
+        }
+    }
+
+    public async Task DeleteByIdAsync(Guid id)
+    {
+        await _unitOfWork.BeginTransactionAsync();
+
+        try
+        {
+            Category category = await _unitOfWork.Repository<Category>().GetByIdAsync(id) ?? throw new KeyNotFoundException(string.Format(BusinessMessage.NotFound_Warning, "Categoria"));
+
+            _unitOfWork.Repository<Category>().Remove(category);
+
+            await _unitOfWork.CommitAsync();
+        }
+        catch
+        {
+            await _unitOfWork.RollbackAsync();
+
+            throw;
+        }
+    }
+
+    public async Task<List<CategoryResponseDTO>> GetAllAsync()
+    {
+        List<Category> categorys = await _unitOfWork.Repository<Category>().GetAllAsync();
+
+        return categorys.Adapt<List<CategoryResponseDTO>>();
+    }
+
+    public async Task<CategoryResponseDTO> GetByIdAsync(Guid id)
+    {
+        Category category = await _unitOfWork.Repository<Category>().GetByIdAsync(id) ?? throw new KeyNotFoundException(string.Format(BusinessMessage.NotFound_Warning, "Categoria"));
+
+        return category.Adapt<CategoryResponseDTO>();
+    }
+
+    public async Task UpdateAsync(Guid id, CategoryUpdateDTO categoryUpdateDTO)
+    {
+        await _unitOfWork.BeginTransactionAsync();
+
+        try
+        {
+            Category category = await _unitOfWork.Repository<Category>().GetByIdAsync(id) ?? throw new KeyNotFoundException(string.Format(BusinessMessage.NotFound_Warning, "Categoria"));
+
+            categoryUpdateDTO.Adapt(category);
+
+            _unitOfWork.Repository<Category>().Update(category);
+
+            await _unitOfWork.CommitAsync();
+        }
+        catch
+        {
+            await _unitOfWork.RollbackAsync();
+
+            throw;
+        }
+    }
+}
---
+++ Backend/Poupe.Domain/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Poupe.Domain.DTOs.Category;
+
+namespace Poupe.Domain.Interfaces;
+
+public interface ICategoryService
+{
+    Task<CategoryResponseDTO> CreateAsync(CategoryCreateDTO CategoryCreateDTO);
+    Task<CategoryResponseDTO> GetByIdAsync(Guid id);
+    Task<List<CategoryResponseDTO>> GetAllAsync();
+    Task UpdateAsync(Guid id, CategoryUpdateDTO CategoryUpdateDTO);
+    Task DeleteByIdAsync(Guid id);
+}
