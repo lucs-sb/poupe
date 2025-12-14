0c742665d052a29c93439c731de65e76dbdb66d7
Lucas <soares0lucas8@gmail.com>
2025-12-14 10:28:03 -0300

    feat: cofiguração do banco de dados sql server

--- Backend/Poupe.API/Program.cs
+++ Backend/Poupe.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Poupe.CrossCutting.IoC;
+using Poupe.Domain.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure Entity Framework and SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PoupeDatabase")));
+
+// Dependences Injections
+builder.Services.AddSettings(builder.Configuration);
+
 var app = builder.Build();
+
+// Apply pending migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Backend/Poupe.CrossCutting/IoC/PipelineExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Poupe.Domain.Settings;
+
+namespace Poupe.CrossCutting.IoC;
+
+public static class PipelineExtensions
+{
+    public static void AddSettings(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<PoupeDatabaseSettings>(configuration.GetSection("PoupeDatabase"));
+    }
+}
