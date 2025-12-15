33a9f33268daeb5f120fad235d04c7a1e18ec29f
Lucas <soares0lucas8@gmail.com>
2025-12-14 22:14:22 -0300

    feat: mapeamento

--- Backend/Poupe.API/Mappers/MappingConfigurations.cs
+++ Backend/Poupe.API/Mappers/MappingConfigurations.cs
@@ -5,6 +5,7 @@ public static class MappingConfigurations
     public static IServiceCollection RegisterMaps(this IServiceCollection services)
     {
         services.RegisterCategoryMaps();
+        services.RegisterTransactionMaps();
         services.RegisterUserMaps();
 
         return services;
