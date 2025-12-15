4d14d562a987f583078a5f3a4620172593ec4206
Lucas <soares0lucas8@gmail.com>
2025-12-14 22:14:44 -0300

    feat: models e dtos de transação

---
+++ Backend/Poupe.API/Models/Transaction/CreateTransactionModel.cs
@@ -0,0 +1,22 @@
+﻿using Poupe.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace Poupe.API.Models.Transaction;
+
+public class CreateTransactionModel
+{
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("value")]
+    public decimal Value { get; set; }
+
+    [JsonPropertyName("type")]
+    public TransactionType Type { get; set; }
+
+    [JsonPropertyName("categoryId")]
+    public string? CategoryId { get; set; }
+
+    [JsonPropertyName("userId")]
+    public string? UserId { get; set; }
+}
---
+++ Backend/Poupe.API/Models/Transaction/UpdateTransactionModel.cs
@@ -0,0 +1,19 @@
+﻿using Poupe.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace Poupe.API.Models.Transaction;
+
+public class UpdateTransactionModel
+{
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("value")]
+    public decimal Value { get; set; }
+
+    [JsonPropertyName("type")]
+    public TransactionType Type { get; set; }
+
+    [JsonPropertyName("categoryId")]
+    public string? CategoryId { get; set; }
+}
